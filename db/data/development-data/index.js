175f6d443b79ca8ca1ebd641eb0c1aa9fff1775e
richnw <richardcawdron@hotmail.com>
2022-12-12 11:55:16 +0000

    add complete 2-project-setup

--- db/data/development-data/index.js
+++ db/data/development-data/index.js
@@ -1,4 +1,12 @@
-exports.categoryData = require('./categories.js');
-exports.commentData = require('./comments.js');
-exports.reviewData = require('./reviews.js');
-exports.userData = require('./users.js');
+const { Pool } = require("pg");
+
+exports.categoryData = require("./categories.js");
+exports.commentData = require("./comments.js");
+exports.reviewData = require("./reviews.js");
+exports.userData = require("./users.js");
+
+if (!process.env.PGDATABASE) {
+  throw new Error("No PGDATABASE configured");
+}
+
+module.exports = new Pool();
