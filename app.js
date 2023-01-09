d160cfe66866d4f931d023a2409c070739c73a27
richnw <richardcawdron@hotmail.com>
2023-01-09 10:58:28 +0000

    allow CORS

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const {
   getCategories,
   getReviewById,
@@ -17,6 +18,7 @@ const {
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.get("/api/categories", getCategories);
 app.get("/api/reviews/:review_id", getReviewById);
