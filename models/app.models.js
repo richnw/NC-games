7ea7d44bf78e236a3279c0cc42f56e448c03091c
richnw <richardcawdron@hotmail.com>
2023-01-13 12:29:51 +0000

    change comment_count from string to number

--- models/app.models.js
+++ models/app.models.js
@@ -22,7 +22,7 @@ function selectReview(review_id) {
 function selectReviews() {
   return db
     .query(
-      "SELECT reviews.review_id, reviews.created_at, reviews.votes, owner, title, category, review_img_url, designer, COUNT(comment_id) AS comment_count FROM reviews LEFT JOIN comments ON reviews.review_id = comments.review_id GROUP BY reviews.review_id, comments.review_id ORDER BY reviews.created_at DESC;"
+      "SELECT reviews.review_id, reviews.created_at, reviews.votes, owner, title, category, review_img_url, designer, COUNT(comment_id)::int AS comment_count FROM reviews LEFT JOIN comments ON reviews.review_id = comments.review_id GROUP BY reviews.review_id, comments.review_id ORDER BY reviews.created_at DESC;"
     )
     .then((result) => result.rows);
 }
